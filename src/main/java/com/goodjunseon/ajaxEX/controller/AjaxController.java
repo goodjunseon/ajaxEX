db40ea2c743843eeeeecee46b028a03870af1b24
goodjunseon <pzs2001926@gmail.com>
2025-03-20 12:17:13 +0900

    ajax-ex-03

---
+++ src/main/java/com/goodjunseon/ajaxEX/controller/AjaxController.java
@@ -0,0 +1,21 @@
+package com.goodjunseon.ajaxEX.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AjaxController {
+    @GetMapping("/ex01")
+    public String ex01() {
+        System.out.println("AjaxController.ex01");
+        return "index"; // index.html에 작성된 내용이 출력됨
+    }
+
+    @PostMapping("/ex02")
+    public @ResponseBody String ex02() {
+        System.out.println("AjaxController.ex02");
+        return "안녕하세요"; // 리턴값이 그대로 출력됨
+    }
+}
